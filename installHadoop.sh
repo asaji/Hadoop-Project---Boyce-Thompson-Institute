413621b0c95d4a92720e85538048542179a1e97c
Akhil <asaji@feilab.(none)>
2013-06-26 16:37:17 -0400

    Finished configuration code for hadoop-env.sh, /etc/hosts, and creation of HDFS directories

--- installHadoop.sh
+++ installHadoop.sh
@@ -8,8 +8,9 @@
 #Please keep a copy of the hadoop install tar.gz; if you recieve a wget error place the tar.gz file in your home directory and delete line 25.
 
 #Configuration Variables
-RSA_PUBLIC_KEY = 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDI5tXt3FJZ8x+XezeSmSFiWJHCof1lR39SO8WKXufJD7rPXU1eK8FqWCwQM7ijGaCloEYLxbPXiY8r7HnU66huMTlDYH8ZAT6oxjY+VTv+55iRDB7R7RuQdnOmzpV6khUHxEXuhrPTBkAeno6xNa6IO0xX9tBL3GRqgh8LAk6Inwz64Jr9ACoKRBBrgM9R2cqbKSvzVaVn4cEi207cfkBJiPM+PqtqdzhLJRyXoVY7meNN/J0e7XQJ2uCx1FH7fpmPfi5ca+eQeNQrKX5WxSMZcyLCb7pXfdweffjC7sYGU+WfeJA3wNnjZO08vKysgOc1MITEz/WR0uXH/mKYMI2t hduser@shiv0'
-HADOOP_INSTALL_LOCATION = http://asaji.webfactional.com/file_stack/linuxRepo/hadoop-1.1.2.tar.gz
+RSA_PUBLIC_KEY='AAAAB3NzaC1yc2EAAAADAQABAAABAQDI5tXt3FJZ8x+XezeSmSFiWJHCof1lR39SO8WKXufJD7rPXU1eK8FqWCwQM7ijGaCloEYLxbPXiY8r7HnU66huMTlDYH8ZAT6oxjY+VTv+55iRDB7R7RuQdnOmzpV6khUHxEXuhrPTBkAeno6xNa6IO0xX9tBL3GRqgh8LAk6Inwz64Jr9ACoKRBBrgM9R2cqbKSvzVaVn4cEi207cfkBJiPM+PqtqdzhLJRyXoVY7meNN/J0e7XQJ2uCx1FH7fpmPfi5ca+eQeNQrKX5WxSMZcyLCb7pXfdweffjC7sYGU+WfeJA3wNnjZO08vKysgOc1MITEz/WR0uXH/mKYMI2t hduser@shiv0'
+HADOOP_INSTALL_PACKAGE=http://asaji.webfactional.com/file_stack/linuxRepo/hadoop-1.1.2.tar.gz
+HOST_NAME=shiv2
 
 #Create Groups/Users
 echo Attempting to create hadoop user and group
@@ -19,7 +20,7 @@ sudo adduser --ingroup hadoop hduser
 #Enable SSH Authentication for Master Node
 cd ~
 sudo cat > authorized_keys << EOF
-ssh-rsa $RSA_PUBLIC_KEY
+ssh-rsa ${RSA_PUBLIC_KEY}
 EOF
 sudo mkdir /home/hduser/.ssh/
 sudo mv authorized_keys /home/hduser/.ssh/
@@ -29,7 +30,7 @@ echo Copied hduser:master id_rsa.pub to authorized_keys
 #Download/Install Hadoop
 echo Downloading Hadoop...
 cd ~
-wget $HADOOP_INSTALL_LOCATION
+wget ${HADOOP_INSTALL_PACKAGE}
 tar -xvf hadoop-1.1.2.tar.gz
 sudo mv hadoop-1.1.2 /usr/local/hadoop
 echo Hadoop Installed. Configuring...
@@ -60,3 +61,13 @@ fi
   mv /usr/local/hadoop/conf/hadoop-env.sh.new /usr/local/hadoop/conf/hadoop-env.sh
   
 echo conf/hadoop-env.sh updated.
+
+  #Configure /etc/hosts
+sudo cp /etc/hosts /etc/hosts.old
+sudo bash -c "sed -e 's:127.0.0.1\slocalhost:127.0.0.1  ${HOST_NAME}:;s:127.0.1.1\s${HOST_NAME}::' /etc/hosts.old > /etc/hosts"
+echo /etc/hosts updated.
+
+  #Create HDFS Directories
+sudo mkdir -p /app/hadoop/tmp
+sudo chown hduser:hadoop /app/hadoop/tmp
+sudo chmod 750 /app/hadoop/tmp
