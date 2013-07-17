340752cf49dd03083a2a269e7c350bdd673ee8d4
Akhil A. Saji <asajinx@gmail.com>
2013-07-17 15:45:51 -0400

    Finished reduce.pl

--- mapReduce/countWords/reduce.pl
+++ mapReduce/countWords/reduce.pl
@@ -2,30 +2,36 @@
 
 use 5.010;
 use warnings;
-use strict;
 
-my $currentWord = null;
+my $currentWord = "";
 my $currentCount = 0;
 
-my $line = "apple	1";
+##Use this block for testing the reduce script with some test data.
+#Open the test file
+#open(my $fh, "<", "testdata.txt");
+#while(!eof $fh) {}
 
-#Remove the \n
-chomp $line;
+while($line = <>) {
+	#Remove the \n
+	chomp $line;
 
-#Index 0 is the word, index 1 is the count value
-my @lineData = split('\t', $line);
-my $word = $lineData[0];
-my $count = $lineData[1];
+	#Index 0 is the word, index 1 is the count value
+	my @lineData = split('\t', $line);
+	my $word = $lineData[0];
+	my $count = $lineData[1];
 
-if($currentWord eq $word) {
-	$currentCount = $currentCount + $count;
-} else {
-	if($currentWord) {
-		#Output the key we're finished working with
-		print "$currentWord \t $currentCount";
-
+	if($currentWord eq $word) {
+		$currentCount = $currentCount + $count;
+	} else {
+		if($currentWord ne "") {
+			#Output the key we're finished working with
+			print "$currentWord \t $currentCount \n";
+		}
 		#Switch the current variables over to the next key
 		$currentCount = $count;
 		$currentWord = $word;
 	}
 }
+
+#deal with the last loop 
+print "$currentWord \t $currentCount \n";
