3555ef2236fbdacd4d92fe54cc14fd113674e551
Akhil A. Saji <asajinx@gmail.com>
2013-07-24 14:36:01 -0400

    Finished GBS File Converter

--- mapReduce/csvMR/gbsConverter.pl
+++ mapReduce/csvMR/gbsConverter.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#gbsConverter.pl by Akhil A. Saji (asaji@bu.edu)
+#gbsConverter.pl by Akhil A. Saji (asajinx@gmail.com)
 #Converts GBS files into plaintext for working with in MapReduce
 
 #Declare dependencies
@@ -12,22 +12,36 @@ use warnings;
 use Tie::Array::CSV;
 
 #Declare input filename
-my $inputCSV = 'gbsTestData.csv';
+my $inputCSV = $ARGV[0];
+my $outputFile = $ARGV[1];
 
-#Use Tie Module
+#Use Tie Module to create multidimensional array.
 tie(my @csvFile, 'Tie::Array::CSV', $inputCSV);
 
 #Find length of the headers column so we know the width of the array
-my $rowCount = $#csvFile+1;
-my $columnCount = $#{$csvFile[1]}+1;
+my $rowCount = $#csvFile;
+my $columnCount = $#{$csvFile[1]};
 
 #Open output filehandle.
-open(my $fh, '>', 'gbsMRoutput.txt');
+open my $fh, ">", $outputFile or die "$0: open $outputFile: $!";
 
 #Default Row to Start from since 0 is headers.
-my $currentRow = 1;
-
 #Loop
-for(my $x=0; $x <= $columnCount; $x++) {
-	print $fh $csvFile[0][$x],":",$csvFile[$currentRow][0],",",$csvFile[$currentRow][$x],"\n";
+for(my $currentRow=1; $currentRow <= $rowCount; $currentRow++) {
+	for(my $y=1; $y <= $columnCount; $y++) {
+		my $organism = $csvFile[0][$y];
+		my $SNP = $csvFile[$currentRow][0];
+		my $value = $csvFile[$currentRow][$y];
+		print $fh "$organism:$SNP,$value\n";
+	}
+	#We have to print to STDERR instead of regular print because regular print goes to the IO Buffer
+	#IO Buffer will not print until the Buffer is full.
+	print STDERR "Processed Row: $currentRow/$rowCount \n";
 }
+
+#close file handle
+close $fh;
+
+#Complete Message
+say "";
+say "Done processing ", $inputCSV, ".";
